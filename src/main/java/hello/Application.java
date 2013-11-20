0a90b2d63ae3782430180084c8c8d4767cc6ed53
snowdream <yanghui1986527@gmail.com>
2013-11-20 22:25:10 +0800

    [Add]Update gradle scripts

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -12,12 +12,12 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
-
+    
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
+        
         System.out.println("Let's inspect the beans provided by Spring Boot:");
-
+        
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
@@ -26,4 +26,3 @@ public class Application {
     }
 
 }
-
