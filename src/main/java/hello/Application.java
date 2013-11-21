4211fbb01e96b62a7cb56d8dc13bf3792e4eb095
snowdream <yanghui1986527@gmail.com>
2013-11-21 17:51:43 +0800

    [Test] Building a RESTful Web Service with Spring Boot Actuator

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,10 +1,7 @@
 package hello;
 
-import java.util.Arrays;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,17 +9,9 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
-    
+
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-        
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        SpringApplication.run(Application.class, args);
     }
 
-}
+} 
