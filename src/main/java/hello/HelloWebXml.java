1627db62f84c49e1c73d41dfbba85b80e79bf302
snowdream <yanghui1986527@gmail.com>
2013-11-28 09:48:57 +0800

    [Add]Converting a Spring Boot JAR Application to a WAR

---
+++ src/main/java/hello/HelloWebXml.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.SpringBootServletInitializer;
+
+public class HelloWebXml extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
