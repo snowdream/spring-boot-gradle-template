c23119fb6a70317d6824e25e1a3d002f6870d604
snowdream <yanghui1986527@gmail.com>
2013-11-29 18:03:27 +0800

    [Add] sercurity codes

---
+++ src/main/java/hello/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/hello-world").permitAll()
+                .anyRequest().authenticated();
+        http
+                .formLogin()
+                .defaultSuccessUrl("/hello")
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+        authManagerBuilder.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
